8a08ba39fbc81a58e6caea7637e04e106715618e
bdickason <dickason@gmail.com>
2014-04-12 14:47:24 -0400

    Added empty model for shots

---
+++ shots-ios/shotModel.h
@@ -0,0 +1,13 @@
+//
+//  shotModel.h
+//  shots-ios
+//
+//  Created by Bradley Dickason on 4/12/14.
+//  Copyright (c) 2014 Bradley Dickason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface shotModel : NSObject
+
+@end
---
+++ shots-ios/shotModel.m
@@ -0,0 +1,13 @@
+//
+//  shotModel.m
+//  shots-ios
+//
+//  Created by Bradley Dickason on 4/12/14.
+//  Copyright (c) 2014 Bradley Dickason. All rights reserved.
+//
+
+#import "shotModel.h"
+
+@implementation shotModel
+
+@end
