6ba8852bbdc0954987b08aee4f8348c07361ac9b
bdickason <dickason@gmail.com>
2014-04-13 00:42:15 -0400

    Added Parse support for data persistance

--- Podfile
+++ Podfile
@@ -3,3 +3,4 @@ platform :ios, '7.0'
 xcodeproj 'shots-ios'
 
 pod 'Firebase', '~> 1.1'
+pod 'Parse', '~> 1.2'
--- shots-ios/shareViewController.m
+++ shots-ios/shareViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "shareViewController.h"
+#import "cfg/env.h"
+
+#import "Parse/Parse.h"
 
 @implementation shareViewController
 @synthesize image;
@@ -17,6 +20,11 @@
 	// Do any additional setup after loading the view, typically from a nib.
     [imageView setImage: image];
     
+    [Parse setApplicationId:PARSE_APP_ID clientKey:PARSE_KEY];
+    
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
 }
 
 - (void)didReceiveMemoryWarning
